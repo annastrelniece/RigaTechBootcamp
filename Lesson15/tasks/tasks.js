fc500f04da8fa20f293838fd5fd2cd7c548016ec
annastrelniece <78434097+annastrelniece@users.noreply.github.com>
2021-03-10 08:46:46 +0200

    Create tasks.js

---
+++ Lesson15/tasks/tasks.js
@@ -0,0 +1,23 @@
+const name = 'Anna';
+const address = 'Rīga, Čiekurkalns';
+const number = 7;
+
+console.log(`Hello, my name is ${name}, I live in ${address} and my favourite number is ${number}!`);
+
+const cars = [
+    'Vaz',
+    'Zaz',
+    'Gaz',
+    'Uaz',
+    'Maz',
+    ];
+
+console.log(`5th car is ${cars [4]}`);
+console.log(`4th car is ${cars [3]}`);
+console.log(`3rd car is ${cars [2]}`);
+console.log(`2nd car is ${cars [1]}`);
+console.log(`1st car is ${cars [0]}`);
+
+//why does the code not work when --
+//const cars = ['vaz', 'zaz', 'gaz','maz','uaz', 'bmw'];
+//console.log(`5th car is` cars[4]);
