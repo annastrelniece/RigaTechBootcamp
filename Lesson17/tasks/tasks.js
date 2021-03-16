be47190dde54eb73f25a66502da0328c4cbba080
annastrelniece <78434097+annastrelniece@users.noreply.github.com>
2021-03-16 18:32:52 +0200

    Update tasks.js

--- Lesson17/tasks/tasks.js
+++ Lesson17/tasks/tasks.js
@@ -1,9 +1,9 @@
 // Task 1 - print out in console if number is odd or even
-const num1 = 35;
+const num1 = 388;
 if (num1 % 2 === 0) {
     console.log(`This number (${num1}) is even`);
 } else {
-    console.log(`This number (${num1}) is odd`);
+    console.log(`This number is odd`);
 }
 
 // Task 2 - print out the age group I'm in
@@ -20,16 +20,27 @@ if (myAge >= 0 && myAge <= 18) {
     console.log(`My age is 51+ years (${myAge})`);
 }
 // Task 3 - FizBazz
-console.log('Task 3: Fizz Bazz for numbers from 15 to 1')
-const fizzBazz = 15;
-for (i = fizzBazz; i > 0; i--) {
-    if (i % 2 === 0 && i % 3 === 0) {
-        console.log('Fizz Bazz');
-    } else if (i % 3 == 0) {
-        console.log('Bazz');
-    } else if (i % 2 == 0) {
-        console.log('Fizz');
-    } else {
-        console.log(i);
-    }
+//console.log('Task 3: Fizz Bazz for numbers from 15 to 1')
+//const fizzBazz = 15;
+//for (i = fizzBazz; i > 0; i--) {
+   // if (i % 2 === 0 && i % 3 === 0) {
+     //   console.log('Fizz Bazz');
+    //} else if (i % 3 == 0) {
+    //    console.log('Bazz');
+    //} else if (i % 2 == 0) {
+   //     console.log('Fizz');
+   // } else {
+   //     console.log(i);
+   // }
+//}
+const number2 = 123123;
+if (number2 % 2 === 0 && number2 % 3 === 0) {
+    console.log('Fizz Bazz');
+} else if (number2 % 2 === 0) {
+    console.log('Fizz');
+} else if (number2 % 3 === 0) {
+    console.log('Bazz');
+} else {
+    console.log(number2);
 }
+
